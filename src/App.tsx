8d7c000c5e5e6b5a8c0af0b9b5ed705d9b0b99c8
Siddhant Jadhav <jadhavsidd123@gmail.com>
2022-12-30 11:37:54 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
 import { Home, ContactUs } from './components';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/contact-us" element={<ContactUs />} />
+    //   </Routes>
+    // </Router>
+    <Home />
   );
 }
 
